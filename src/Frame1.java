3cd854754eaba5b81bee807b42b52e4b4b8ec9f7
YeoWool <gkaudrl542@daum.net>
2018-03-15 16:29:26 +0900

    file upload code & add some information about code

--- src/Frame1.java
+++ src/Frame1.java
@@ -1,20 +1,25 @@
+import java.awt.AWTException;
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-
-import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
-import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 
 public class Frame1 extends Frame implements ActionListener
@@ -53,24 +58,66 @@ public class Frame1 extends Frame implements ActionListener
                     String format = "jpg";
 
                     while (running) {
-                        robot.delay(1000);
+                        robot.delay(1000*60*10); // 10분
                         String fileName = num + "FullScreenshot." + format;
                         num = num + 1;
 
                         Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
                         BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-                        ImageIO.write(screenFullImage, format, new File(fileName));
 
-                        System.out.println("A full screenshot saved!"+num);
-                    }
+                        // 서버에 파일 전송 시 파일 객체 이용- 이때 이미지가 경로에 저장되어 있어야 한다. - 그냥 파일 이름 써도 될거 같은데?
+                        //ImageIO.write(screenFullImage, format, new File(fileName));
+                        //System.out.println("A full screenshot saved!"+num);
+
+                        String urlToConnect = "http://13.125.139.100/image";
+                        File fileToUpload = new File(fileName);
+                        String boundary = Long.toHexString(System.currentTimeMillis());//Just generate some unique
+
+                        URLConnection connection = new URL(urlToConnect).openConnection();
+                        connection.setDoOutput(true);
+                        connection.setRequestProperty("Content-Type","multipart/form-data; boundary=" + boundary);
+                        PrintWriter writer = null;
+
+                        try{
+                            writer = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
+                            writer.println("--" + boundary);
+                            writer.println("Content-Disposition: form-data; name=\"picture\"; filename=\"bla.jpg\"");
+                            writer.println("Content-Type: image/jpeg");
+                            writer.println();
+                            BufferedReader reader = null;
+
+                            try{
+                                reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileToUpload)));
+                                for(String line; (line = reader.readLine()) != null;){
+                                    writer.println(line);
+                                }
+
+                            }finally {
+                                if(reader != null) try {reader.close();} catch(IOException logOrIgnore) {}
+                            }
+                            writer.println("--");
+
+                        } finally {
+                            if(writer != null) writer.close();
+                        }
+
+                        int responseCode = ((HttpURLConnection) connection).getResponseCode();
+                        if( responseCode == 200) System.out.println("OK");
+                        else System.out.println("fail");
+
+                    }// end while
 
                 } catch (AWTException | IOException ex) {
                     System.err.println(ex);
                 }
-            }}
-            );
+             }}//end Runnable
+            ); // end capture_thread
+
         capture_thread.start();
-    }
+
+    }// end startThread
+
+
 
 
     @Override
